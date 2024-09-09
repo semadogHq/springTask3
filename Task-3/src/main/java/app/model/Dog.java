2e5b704a893341c002a48754c325033728ca6688
semadogHq <sv16041@gmail.com>
2024-09-09 09:21:03 +0300

    Add files via upload

---
+++ Task-3/src/main/java/app/model/Dog.java
@@ -0,0 +1,11 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+    public String toString() {
+        return "Dog";
+    }
+
+}
